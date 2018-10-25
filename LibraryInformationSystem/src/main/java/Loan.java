07e22463f4dfdcaae366c44c7e2528e823675f85
Martin Piatka <chachacha2323@gmail.com>
2018-10-25 20:24:52 +0200

    Add Loan class

---
+++ LibraryInformationSystem/src/main/java/Loan.java
@@ -0,0 +1,49 @@
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ *
+ * @author Martin Piatka
+ */
+
+public class Loan {
+    int id;
+    Customer customer;
+    ZonedDateTime timestamp;
+    List<LoanItem> items;
+
+    public Loan(int id, Customer customer, ZonedDateTime timestamp) {
+        this.id = id;
+        this.customer = customer;
+        this.timestamp = timestamp;
+    }
+
+    public boolean addLoanedBook(Book book){
+        if(book == null)
+            return false;
+
+        BookCondition borrowCondition = BookCondition.UNKNOWN; // Will be taken from book
+        BookCondition returnCondition = BookCondition.UNKNOWN; // Not yet returned
+
+        LoanItem item = new LoanItem(book, borrowCondition, returnCondition);
+        items.add(item);
+
+        return true;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(ZonedDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
