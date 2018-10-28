3b506f19439b8f46bbabd3ba98bc222e6da82854
matus.congrady <matus.congrady@cellense.com>
2018-10-28 20:18:49 +0100

    improve DAO interface

--- LibraryInformationSystem/src/main/java/Interfaces/DAO.java
+++ LibraryInformationSystem/src/main/java/Interfaces/DAO.java
@@ -1,31 +1,46 @@
 package Interfaces;
 
 import java.util.List;
-import java.util.Optional;
+
+/**
+ *
+ * @author Matúš Čongrády
+ */
 
 public interface DAO<T> {
     /**
-     * Gets entity with given id
-     * @param id
-     * @return
+     * Creates a persistent representation of an entity in the database.
+     *
+     * @param entity entity to be persisted
      */
-    Optional<T> get(long id);
+    public void create(T t);
 
     /**
-     * Gets all entities
-     * @return List containing all entities
+     * Updates an entity in the database.
+     *
+     * @param entity entity to be updated
      */
-    List<T> getAll();
+    public void update(T entity);
 
     /**
-     * Saves entity to database
-     * @param t
+     * Deletes an entity from the database.
+     *
+     * @param entity to be deleted
      */
-    void save(T t);
+    public void remove(T entity);
 
     /**
-     * Removes entity from database
-     * @param t
+     * Finds an entity in the database by the given ID.
+     *
+     * @param id ID of the entity to be found
+     * @return an entity with the given ID
      */
-    void delete(T t);
+    public T findById(Long id);
+
+    /**
+     * Returns a list of all entities in the database.
+     *
+     * @return a list of all entities in the database
+     */
+    public List<T> findAll();
 }
