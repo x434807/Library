cb76aa5a4ef312be2b2b6233324bf8bc4f6b049f
Andrej Sokolík <434807@mail.muni.cz>
2018-11-22 17:21:01 +0100

    ServiceLayer

---
+++ servicelayer/src/main/java/cz/muni/fi/pa165/skupina06/team01/servicelayer/config/ServiceConfig.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.skupina06.team01.servicelayer.config;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.PersistenceApplicationContext;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.dto.BookDTO;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.dto.CustomerDTO;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.dto.LoanDTO;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.entity.Book;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.entity.Customer;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.entity.Loan;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *
+ * @author Andrej Sokolík
+ */
+@Configuration
+@Import(PersistenceApplicationContext.class)
+@ComponentScan(basePackageClasses = {
+    /*PersonServiceImpl.class, 
+    PersonFacadeImpl.class,
+    AbilityServiceImpl.class,
+    AbilityFacadeImpl.class,
+    GhostServiceImpl.class,
+    GhostFacadeImpl.class,
+    HouseServiceImpl.class,
+    HouseFacadeImpl.class*/})
+public class ServiceConfig {
+
+    @Bean
+    public Mapper dozer() {
+        DozerBeanMapper dozer = new DozerBeanMapper();
+        dozer.addMapping(new DozerCustomConfig());
+        return dozer;
+    }
+    
+    public class DozerCustomConfig extends BeanMappingBuilder {
+		@Override
+		protected void configure() {
+			mapping(Customer.class, CustomerDTO.class);
+			mapping(Book.class, BookDTO.class);
+			mapping(Loan.class, LoanDTO.class);
+		}
+	}
+}
