f3b9f9bfd841c6245f85566dac04e3d90e9334b9
matus.congrady <matus.congrady@cellense.com>
2018-11-26 10:19:14 +0100

    add BookDTO

--- api/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/dto/BookDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/dto/BookDTO.java
@@ -5,14 +5,124 @@
  */
 package cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.dto;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.entity.Customer;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.enums.BookCondition;
+
 /**
  *
- * @author 
+ * @author Matúš Čongrády
  */
 public class BookDTO {
+    private long id;
+    private String ISBN;
+    private String name;
+    private String author;
+    private BookCondition condition;
+    private boolean isAvailable = true;
+    private Customer customer;
 
-    boolean getId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public long getId() {
+        return id;
     }
-    
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BookCondition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(BookCondition condition) {
+        this.condition = condition;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BookDTO bookDTO = (BookDTO) o;
+
+        return new EqualsBuilder()
+                .append(id, bookDTO.id)
+                .append(isAvailable, bookDTO.isAvailable)
+                .append(ISBN, bookDTO.ISBN)
+                .append(name, bookDTO.name)
+                .append(author, bookDTO.author)
+                .append(condition, bookDTO.condition)
+                .append(customer, bookDTO.customer)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(ISBN)
+                .append(name)
+                .append(author)
+                .append(condition)
+                .append(isAvailable)
+                .append(customer)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+                "id=" + id +
+                ", ISBN='" + ISBN + '\'' +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", condition=" + condition +
+                ", isAvailable=" + isAvailable +
+                ", customer=" + customer +
+                '}';
+    }
+
 }
