82e94b82683ed55a4cc3d374082bcfe32cdeb55d
matus.congrady <matus.congrady@cellense.com>
2018-11-26 13:10:17 +0100

    adjust book service interface

--- servicelayer/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/service/BookService.java
+++ servicelayer/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/service/BookService.java
@@ -20,8 +20,8 @@ public interface BookService {
      * Creates a new Book in the system.
      *
      * @param book to be created
-     * @throws DataAcessException       when there is any problem in accessing Book
-     * @throws IllegalArgumentException if the book is not of Book class or is null
+     * @throws DataAcessException       in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the Book is null or not present in the database
      */
     void createBook(Book book) throws DataAccessException, IllegalArgumentException;
 
@@ -29,8 +29,8 @@ public interface BookService {
      * Removes the given Book by his ID.
      *
      * @param book is a Book which is going to be removed
-     * @throws DataAcessException       when there is any problem in accessing Book
-     * @throws IllegalArgumentException if Book is not of Book class or is null
+     * @throws DataAcessException       in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the Book is null or not present in the database
      */
     void removeBook(Book book) throws DataAccessException, IllegalArgumentException;
 
@@ -38,8 +38,8 @@ public interface BookService {
      * Updates the given Book.
      *
      * @param book the Book to be updated
-     * @throws DataAcessException       when there is any problem with accessing Book
-     * @throws IllegalArgumentException if Book is not of Book class or is null
+     * @throws DataAcessException       in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the Book is null or not present in the database
      */
     void updateBook(Book book) throws DataAccessException, IllegalArgumentException;
 
@@ -48,26 +48,26 @@ public interface BookService {
      *
      * @param id the Book's ID
      * @return book with the given id
-     * @throws DataAcessException when there is any problem with accessing Book
-     * @throws IllegalArgumentException if Book is not Book class or is null
+     * @throws DataAcessException in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the id is null or not present in the database
      */
     Book findBookById(Long id) throws DataAccessException, IllegalArgumentException;
 
     /**
-     * Finds a Book by her/his ID.
+     * Finds a Book by its ID.
      *
-     * @param id the Book's ID
-     * @return book with the given id
-     * @throws DataAcessException when there is any problem with accessing Book
-     * @throws IllegalArgumentException if Book is not Book class or is null
+     * @param name the name of the book
+     * @return book with the given name
+     * @throws DataAcessException in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the Book is null or not present in the database
      */
-    Book findBookByName(Long id) throws DataAccessException, IllegalArgumentException;
+    Book findBookByName(String name) throws DataAccessException, IllegalArgumentException;
 
     /**
      * Returns a list of all Books in the system.
      *
      * @return the list of all Books
-     * @throws DataAcessException when there is any problem with accessing Books
+     * @throws DataAcessException in case of any failure on the persistence layers
      */
     List<Book> getAllBooks() throws DataAccessException;
 
@@ -75,7 +75,7 @@ public interface BookService {
      * Finds all loans of a specific Book
      * 
      * @return list of all books that need revision (have condition BAD or UNKOWN)
-     * @throws DataAcessException       when there is any problem with accessing Books
+     * @throws DataAcessException in case of any failure on the persistence layers
      */
     List<Book> getAllBooksThatNeedRevision() throws DataAccessException, IllegalArgumentException;
 }
