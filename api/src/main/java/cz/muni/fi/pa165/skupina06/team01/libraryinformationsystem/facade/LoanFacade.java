0d13d6accf9e3b1de2080ac42330fc704cac1af1
Martin Piatka <chachacha2323@gmail.com>
2018-11-26 16:05:05 +0100

    Add javadoc to loan service and facade

--- api/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/facade/LoanFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/facade/LoanFacade.java
@@ -14,12 +14,55 @@ import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.exceptions.Boo
 
 public interface LoanFacade {
 
+    /**
+     * Adds book into a loan
+     *
+     * @param loan
+     * @param book
+     * @throws BookNotAvailableException - when book is not available
+     */
     void addLoanedBook(LoanDTO loan, BookDTO book) throws BookNotAvailableException;
+
+    /**
+     * Creates a loan with multiple books for a customer
+     * @param customer
+     * @param books
+     * @return created loan
+     * @throws BookNotAvailableException - when at least one book is not available
+     */
     LoanDTO loanBooks(CustomerDTO customer, List<BookDTO> books) throws BookNotAvailableException;
 
+    /**
+     * Finds loan by id
+     * @param id
+     * @return loan
+     */
     LoanDTO findById(Long id);
+
+    /**
+     * Returns all loans
+     * @return all loans
+     */
     List<LoanDTO> findAll();
+
+    /**
+     * Creates a new loan in the system
+     *
+     * @param loan
+     */
     void create(LoanDTO loan);
+
+    /**
+     * Removes the specified loan from the system
+     *
+     * @param loan
+     */
     void remove(LoanDTO loan);
+
+    /**
+     * Updates the specified loan
+     *
+     * @param loan
+     */
     void update(LoanDTO loan);
 }
--- servicelayer/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/service/LoanService.java
+++ servicelayer/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/service/LoanService.java
@@ -16,14 +16,72 @@ import java.util.List;
 
 @Service
 public interface LoanService {
+    /**
+     * Adds book into a loan
+     *
+     * @param loan
+     * @param book
+     * @throws BookNotAvailableException - when book is not available
+     * @throws DataAcessException in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the id is null or not present in the database
+     */
     void addLoanedBook(Loan loan, Book book) throws BookNotAvailableException, DataAccessException, IllegalArgumentException;
 
+    /**
+     * Creates a loan with multiple books for a customer
+     * @param customer
+     * @param books
+     * @return created loan
+     * @throws BookNotAvailableException - when at least one book is not available
+     * @throws DataAcessException in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the id is null or not present in the database
+     */
     Loan loanBooks(Customer customer, List<Book> books) throws BookNotAvailableException, DataAccessException, IllegalArgumentException;
 
+
+    /**
+     * Finds a loan by its ID.
+     *
+     * @param id the Loan's ID
+     * @return loan with the given id
+     * @throws DataAcessException in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the id is null or not present in the database
+     */
     Loan findById(Long id) throws DataAccessException, IllegalArgumentException;
+
+    /**
+     * Returns all loans
+     *
+     * @return loans
+     * @throws DataAcessException in case of any failure on the persistence layer
+     */
     List<Loan> findAll() throws DataAccessException;
+
+    /**
+     * Creates a new Loan in the system.
+     *
+     * @param loan to be created
+     * @throws DataAcessException       in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the Loan is null or not present in the database
+     */
     void create(Loan loan) throws DataAccessException, IllegalArgumentException;
+
+    /**
+     * Removes the specified Loan from the system.
+     *
+     * @param loan to be removed
+     * @throws DataAcessException       in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the Loan is null or not present in the database
+     */
     void remove(Loan loan) throws DataAccessException, IllegalArgumentException;
+
+    /**
+     * Updates the specified Loan
+     *
+     * @param loan to be updated
+     * @throws DataAcessException       in case of any failure on the persistence layer
+     * @throws IllegalArgumentException if the Loan is null or not present in the database
+     */
     void update(Loan loan) throws DataAccessException, IllegalArgumentException;
 
 }
