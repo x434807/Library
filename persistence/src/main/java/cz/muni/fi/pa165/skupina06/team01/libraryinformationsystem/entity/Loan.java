d00dfebfdaf95fb6298ffdbb659e4d0e3d1e3601
matus.congrady <matus.congrady@cellense.com>
2018-11-26 13:10:40 +0100

    remove unused imports from Loan

--- persistence/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/entity/Loan.java
+++ persistence/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/entity/Loan.java
@@ -17,9 +17,6 @@ import javax.persistence.Table;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.enums.BookCondition;
-import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.exceptions.BookNotAvailableException;
-
 /**
  *
  * @author Martin Piatka
