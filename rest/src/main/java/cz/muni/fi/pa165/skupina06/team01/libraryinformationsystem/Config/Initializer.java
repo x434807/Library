98c2c235271cdf0695a1fcceb3da22389d30c777
Martin Piatka <chachacha2323@gmail.com>
2018-12-13 19:45:53 +0100

    Initializer stuff

--- rest/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/Config/Initializer.java
+++ rest/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/Config/Initializer.java
@@ -1,11 +1,22 @@
 package cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.Config;
 import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.ApiContract;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        servletContext.addListener(RequestContextListener.class);
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -19,6 +30,6 @@ public class Initializer extends AbstractAnnotationConfigDispatcherServletInitia
 
     @Override
     protected String[] getServletMappings() {
-        return new String[]{"/"};
+        return new String[]{ApiContract.REST + "/*"};
     }
 }
