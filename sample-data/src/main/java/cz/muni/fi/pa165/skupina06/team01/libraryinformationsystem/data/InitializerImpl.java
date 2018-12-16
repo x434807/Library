8f8a82f4da5b8d7dbfb47beb261b26d9bc1b1685
Martin Piatka <chachacha2323@gmail.com>
2018-12-16 19:41:55 +0100

    Add sample customer and sample book

--- sample-data/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/data/InitializerImpl.java
+++ sample-data/src/main/java/cz/muni/fi/pa165/skupina06/team01/libraryinformationsystem/data/InitializerImpl.java
@@ -1,6 +1,10 @@
 package cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.data;
 
 
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.entity.Book;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.entity.Customer;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.entity.Loan;
+import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.enums.BookCondition;
 import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.service.BookService;
 import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.service.CustomerService;
 import cz.muni.fi.pa165.skupina06.team01.libraryinformationsystem.service.LoanService;
@@ -29,6 +33,41 @@ public class InitializerImpl implements Initializer{
 
     @Override
     public void loadData() {
+        loadBooks();
+        loadCustomers();
+    }
 
+    void loadBooks(){
+        createBook("Tester Testovics", "Testing 101", "3216546-56468", BookCondition.GOOD);
+    }
+
+    void loadCustomers(){
+        createCustomer("Juraj", "Pokazil", "test123", "hunter2");
+    }
+
+    void createLoan(Customer customer){
+
+    }
+
+    void createBook(String author, String title, String ISBN, BookCondition condition){
+        Book book = new Book();
+
+        book.setAuthor(author);
+        book.setName(title);
+        book.setISBN(ISBN);
+        book.setCondition(condition);
+
+        bookService.createBook(book);
+    }
+
+    void createCustomer(String name, String surname, String login, String password){
+        Customer customer = new Customer();
+
+        customer.setName(name);
+        customer.setSurname(surname);
+        customer.setLogin(login);
+        customer.setPassword(password);
+
+        customerService.registerCustomer(customer);
     }
 }
